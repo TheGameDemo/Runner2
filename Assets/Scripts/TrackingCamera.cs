2d6d616a512af12f320c0804ca45c8d527259b33
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-07-08 20:00:14 +0800

    Filling the View

--- Assets/Scripts/TrackingCamera.cs
+++ Assets/Scripts/TrackingCamera.cs
@@ -4,9 +4,15 @@ public class TrackingCamera:MonoBehaviour
 {
     Vector3 offset, position;
 
+    float viewFactorX;
+
     private void Awake()
     {
         offset = transform.localPosition;
+
+        Camera c = GetComponent<Camera>();
+        float viewFactorY = Mathf.Tan(c.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        viewFactorX = viewFactorY * c.aspect;
     }
 
     public void StartNewGame()
@@ -19,4 +25,7 @@ public class TrackingCamera:MonoBehaviour
         position = focusPoint + offset;
         transform.localPosition = position;
     }
+
+    public FloatRange VisibleX(float z) =>
+    FloatRange.PositionExtents(position.x, viewFactorX * (z - position.z));
 }
