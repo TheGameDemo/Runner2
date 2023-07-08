895693d38c6e074d91c06de50f05f7703b65ec2f
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-07-08 19:57:47 +0800

    Float Range

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,18 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Min(0.001f)]
+    float maxDeltaTime = 1f / 120f;
+
+    [SerializeField]
+    Runner runner;
+
+    [SerializeField]
+    TrackingCamera trackingCamera;
+
+    [SerializeField]
+    TextMeshPro displayText;
+
+    bool isPlaying;
+
+    void StartNewGame()
     {
-        
+        trackingCamera.StartNewGame();
+        runner.StartNewGame();
+        isPlaying = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (isPlaying)
+        {
+            UpdateGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartNewGame();
+        }
+    }
+
+    void UpdateGame()
+    {
+        // To enforce precise movement we'll implement a maximum delta time.
+        float accumulateDeltaTime = Time.deltaTime;
+        while (accumulateDeltaTime > maxDeltaTime && isPlaying)
+        {
+            isPlaying = runner.Run(maxDeltaTime);
+            accumulateDeltaTime -= maxDeltaTime;
+        }
+
+        isPlaying = isPlaying && runner.Run(accumulateDeltaTime);
         
+        runner.UpdateVisualization();
+        trackingCamera.Track(runner.Position);
+        displayText.SetText("{0}", Mathf.Floor(runner.Position.x));
     }
 }
